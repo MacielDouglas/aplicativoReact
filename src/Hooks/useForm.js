b594e3e75452b47e0dd410add4ee9be807abace9
MacielDouglas <douglasmaciel.dm@gmail.com>
2023-02-07 15:53:29 -0300

    terminado Post de fotos usuario

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -12,6 +12,11 @@ const types = {
     message:
       'A senha precisa ter 1 caracter maíusculo, 1 minúsculo e um dígito. Com no mínimo 8 caracteres.',
   },
+
+  number: {
+    regex: /^\d+$/,
+    message: 'Utilize números apenas.',
+  },
 };
 const useForm = (type) => {
   const [value, setValue] = React.useState('');
