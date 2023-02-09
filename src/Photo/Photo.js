21589ac7a8758857c4d02ed502acaeff15baa127
MacielDouglas <douglasmaciel.dm@gmail.com>
2023-02-09 18:14:12 -0300

    criado foto página única (single)

--- src/Photo/Photo.js
+++ src/Photo/Photo.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { PHOT_GET } from '../api';
+import Error from '../Helper/Error';
+import Loading from '../Helper/Loading';
+import useFetch from '../Hooks/useFetch';
+import PhotoContent from './PhotoContent';
 
 const Photo = () => {
-  return <div>Photo</div>;
+  const { id } = useParams();
+  const { data, loading, error, request } = useFetch();
+
+  React.useEffect(() => {
+    const { url, options } = PHOT_GET(id);
+    request(url, options);
+  }, [request, id]);
+
+  if (error) return <Error error={error} />;
+  if (loading) return <Loading />;
+  if (data)
+    return (
+      <section className="container mainContainer">
+        <PhotoContent single={true} data={data} />
+      </section>
+    );
+  else return null;
 };
 
 export default Photo;
